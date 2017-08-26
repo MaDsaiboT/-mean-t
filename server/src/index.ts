442c50d2c36176aa331aef2c5ec9eaa30aeb4a09
MaDsaiboT <mad.saibot@gmail.com>
2017-08-26 23:32:40 +0200

    basic express server running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,16 @@
 console.log('works');
 
+import * as express from "express"
+var ex = express();
+ex.get('/', (req, res) => {
+    res.send('jo!');
+});
+
+let port:number = 1337;
+ex.listen(1337, _=>{
+    console.log('Listening on port '+ port);
+});
+
 export class server{
     constructor() {
         
