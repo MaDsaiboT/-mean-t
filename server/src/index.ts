1f09e7b15d672024b4cbedc2ed2d8effd3167e4f
MaDsaiboT <mad.saibot@gmail.com>
2017-08-27 04:23:37 +0200

    basic http server functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,44 @@
-console.log('works');
-
 import * as express from "express"
-var ex = express();
-ex.get('/', (req, res) => {
-    res.send('jo!');
+var path = require('path');
+var app  = express();
+
+console.log('works ');
+
+var clientdir = path.resolve(__dirname,'../../client/dist/',)
+
+console.log(clientdir );
+
+
+
+app.use(express.static(__dirname)); 
+
+app.get('*.js', (req, res) => {
+  let file:string = (clientdir + req.params[0] + '.js' ) 
+  res.sendFile(file, _=>{
+    console.log('send: ' + file);
+  })
+})
+
+app.get('*.png', (req, res) => {
+  res.sendFile(clientdir + req.params[0] + '.png')
+})
+
+app.get('*.ico', (req, res) => {
+  res.sendFile(clientdir + req.params[0] + '.ico')
+})
+
+app.get("/", (req, res)=>{
+  res.sendFile('index.html',{ root: clientdir })
 });
 
-let port:number = 1337;
-ex.listen(1337, _=>{
+
+
+let port:number = 8080;
+
+
+var listen = true;
+if ( listen ){
+  app.listen(port, _=>{
     console.log('Listening on port '+ port);
-});
-
-export class server{
-    constructor() {
-        
-    }
-
+  });
 }
